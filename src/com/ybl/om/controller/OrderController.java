4bc77b3fc47d3c3842f80a22f384ac906e87eb0f
fangln <971399161@qq.com>
2017-08-19 15:20:19 +0800

    modify

--- src/com/ybl/om/controller/OrderController.java
+++ src/com/ybl/om/controller/OrderController.java
@@ -1,5 +1,10 @@
 package com.ybl.om.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ordr.do")
 public class OrderController {
 
 }
