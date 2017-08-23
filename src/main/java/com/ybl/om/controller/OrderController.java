e7ccc9d09825242f3e4aedac2c8b772462d7f683
fangli9713 <971399161@qq.com>
2017-08-23 15:06:19 +0800

    修改配置 并且测试数据库的连通性

--- src/main/java/com/ybl/om/controller/OrderController.java
+++ src/main/java/com/ybl/om/controller/OrderController.java
@@ -1,19 +1,29 @@
-package src.com.ybl.om.controller;
+package com.ybl.om.controller;
 
+import com.ybl.om.dao.UserMapper;
+import com.ybl.om.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class OrderController {
 
+    @Resource
+    private UserMapper userMapper;
+
     @RequestMapping("/order.do")
     @ResponseBody
     public String helloOM(HttpServletRequest request, HttpServletResponse response){
 
+        final User user = userMapper.selectByPrimaryKey(5L);
+        System.out.println(user);
         return "hahahahhahahaha!!!!";
+
+
     }
 }
